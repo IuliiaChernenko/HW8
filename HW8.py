f24456bd71be631269f0d229e8a4359cbca64315
IuliiaChernenko <iuliia.chernenko@gmail.com>
2024-01-23 16:03:11 +0300

    Function is correct

--- HW8.py
+++ HW8.py
@@ -1,12 +1,10 @@
 # объявление функции
 def convert_to_miles(km):
     km = float(km)
-    return km * 0.6214 + 1
+    return km * 0.6214
 
 # считываем данные
 num = 28
-
-print('test')
 
 # вызываем функцию
 print(convert_to_miles(num))
